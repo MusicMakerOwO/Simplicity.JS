e3735162b37a0020dc7eb14c839879f4a14ee2e1
Music Maker <86620902+MusicMakerOwO@users.noreply.github.com>
2023-08-27 00:28:14 -0500

    Added interactionCreate event

--- Utils/EventHandler.js
+++ Utils/EventHandler.js
@@ -1,5 +1,31 @@
 const Format = require('./Format.js');
 
+const Guild = require('../Structures/Guild.js');
+const Message = require('../Structures/Message.js');
+const User = require('../Structures/User.js');
+
+const Channel = require('../Structures/Channels/TextChannel.js');
+const Member = require('../Structures/GuildData/Member.js');
+const Role = require('../Structures/GuildData/Role.js');
+const Emoji = require('../Structures/GuildData/Emoji.js');
+const Sticker = require('../Structures/GuildData/Sticker.js');
+
+const Ping = require('../Interactions/Ping.js');
+const Command = require('../Interactions/Command.js');
+const Button = require('../Interactions/Button.js');
+const SelectMenu = require('../Interactions/SelectMenu.js');
+const Modal = require('../Interactions/Modal.js');
+
+const types = [
+    { type: 'invalid', class: Object },
+    { type: 'ping', class: Ping },
+    { type: 'command', class: Command },
+    { type: 'buttonClick', class: Button },
+    { type: 'selectMenu', class: SelectMenu },
+    { type: 'autocomplete', class: Object },
+    { type: 'modalSubmit', class: Modal }
+]
+
 module.exports = async (client, event, data) => {
     switch (event) {
         case 'MESSAGE_CREATE':
@@ -7,5 +33,16 @@ module.exports = async (client, event, data) => {
         case 'GUILD_CREATE':
             this.client.guilds.set(data.id, data);
             return client.emit('guildCreate', await Format.guild(client, data));
+        case 'INTERACTION_CREATE':
+            // Discord is stupid and combines buttons and select menus into one type >:(
+            if (data.type > 3) data.type++;
+            if ('values' in data.data) data.type = 4;
+
+            let interaction = types[data.type];
+            if (!interaction) return;
+
+            client.emit('interactionCreate', new interaction.class(data, client));
+            client.emit(interaction.type, new interaction.class(data, client));
+            return;
     }
 }
